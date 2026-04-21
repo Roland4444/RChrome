fcedfcce20de63bdc00b54fa50ac9a1b2630a316
Roland4444 <turbopollux@rambler.ru>
2026-04-21 13:09:20 +0400

    test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-// use headless_chrome::{Browser, LaunchOptions};
+use headless_chrome::{Browser, LaunchOptions};
 
 // #[tokio::main]
 // async fn main() -> Result<(), Box<dyn std::error::Error>> {
 //     let options = LaunchOptions::default_builder()
-//         .headless(false)
+//         .headless(true)
 //         .build()
 //         .unwrap();
 
@@ -18,17 +18,49 @@
 
 
 
+// use thirtyfour::prelude::*;
+
+// #[tokio::main]
+// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     let caps = DesiredCapabilities::chrome();
+//     let driver = WebDriver::new("http://localhost:55671", caps).await?; // chromedriver по умолчанию
+
+//     let init_addr = "https://relits.bitrix24.ru/company/personal/user/";
+//     let id = 1;
+//     driver.get(&format!("{}{}", init_addr, id)).await?;
+//     tokio::time::sleep(std::time::Duration::from_secs(30)).await;
+
+//     Ok(())
+// }
+
+
 use thirtyfour::prelude::*;
+use tokio::time::{sleep, Duration};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let caps = DesiredCapabilities::chrome();
-    let driver = WebDriver::new("http://localhost:55671", caps).await?; // chromedriver по умолчанию
+    let mut caps = DesiredCapabilities::chrome();
+    caps.add_arg("--headless")?; 
+    caps.add_arg("--disable-gpu")?; 
+    caps.add_arg("--no-sandbox")?; 
+    caps.add_arg("--disable-dev-shm-usage")?; 
+    caps.add_arg("--window-size=1920,1080")?; 
 
-    let init_addr = "https://relits.bitrix24.ru/company/personal/user/";
-    let id = 1;
-    driver.get(&format!("{}{}", init_addr, id)).await?;
-    tokio::time::sleep(std::time::Duration::from_secs(30)).await;
+    let driver = WebDriver::new("http://localhost:21000", caps).await?;
 
+    driver.get("https://ya.ru").await?;
+    sleep(Duration::from_secs(2)).await;
+
+    let search_box = driver.find(By::Name("text")).await?;
+    search_box.send_keys("Rust programming language").await?;
+
+    search_box.send_keys(Key::Enter).await?;
+
+    driver.query(By::Css("body")).first().await?;
+    sleep(Duration::from_secs(3)).await;
+
+    println!("Title: {:?}", driver.title().await?);
+
+    driver.quit().await?;
     Ok(())
 }
